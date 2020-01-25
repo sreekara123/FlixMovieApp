dd77b3340c0b505496fb7aa01faf8fe376ecb453
Sreekara Yachamaneni <syachama@purdue.edu>
2020-01-25 18:28:28 -0500

    Initial Commit

---
+++ FlixMovieApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlixMovieApp
+//
+//  Created by Sreekara Yachamaneni on 1/25/20.
+//  Copyright © 2020 Sreekara Yachamaneni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
